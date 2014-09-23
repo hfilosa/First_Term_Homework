a388238222b22af52d94dc9f4607f23ea83a703c
Henry Filosa <henryfilosa@henrys-imac.home>
2014-09-23 17:06:25 -0400

    The codingbat homework for 9/23/14. I put some test programs in the Driver.java

---
+++ 05-Coding_Bat/Driver.java
@@ -0,0 +1,9 @@
+public class Driver {
+    public static void main (String[] args){
+	StringStuff exer = new StringStuff();
+	System.out.println(exer.nonStart("stop","go"));
+	System.out.println(exer.makeAbba("stop","go"));
+	System.out.println(exer.diff21(16));
+	System.out.println(exer.diff21(-40));
+	    }
+}
