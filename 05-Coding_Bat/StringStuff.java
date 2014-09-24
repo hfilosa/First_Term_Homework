7f32714b56025dc074309540037349d64d9325df
Henry Filosa <hfilosa@gmail.com>
2014-09-24 08:25:36 -0400

    Added the classwork assigned 9/24/14

--- 05-Coding_Bat/StringStuff.java
+++ 05-Coding_Bat/StringStuff.java
@@ -1,23 +1,39 @@
 public class StringStuff {
 
-//nonStart
+    //HomeWork from 9/23/14
 
 public String nonStart(String a, String b) {
   return a.substring(1) + b.substring(1);
 }
 
-//makeAbba
-
 public String makeAbba(String a, String b) {
   return a+b+b+a;
 }
-
-//diff21
 
 public int diff21(int n) {
   if (n>21) { return 2*(n-21);}
   else if (n>=0) {return 21-n;}
   else {return (-1*n)+21;}
+}
+
+    //Class work from 9/24/14
+
+public boolean nearHundred(int n) {
+  if (Math.abs(100-n)<=10 || Math.abs(200-n)<=10){return true;}
+  else {return false;}
+}
+
+public int teaParty(int tea, int candy) {
+  if (tea<5 || candy<5) {return 0;}
+  else if(tea>=(2*candy) || candy>=(2*tea)){return 2;}
+  else {return 1;}
+}
+
+public boolean lastDigit(int a, int b, int c) {
+ if (a%10 == b%10) {return true;}
+ else if (a%10 == c%10) {return true;}
+ else if (b%10 == c%10) {return true;}
+ else {return false;}
 }
 
 }
