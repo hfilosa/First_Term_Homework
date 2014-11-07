2277d5a4fa04bcffa20e5b018940a1c8a4f3d2e2
Henry Filosa <hfilosa@gmail.com>
2014-11-07 08:18:24 -0500

    For sarry project

---
+++ 15-sarray/sarray.java
@@ -0,0 +1,31 @@
+public class sarray {
+    private int[] data;
+
+    public sarray(){
+    }
+
+    //adds value to end of list
+    public void add(int i){
+    }
+
+    //adds value at specific index and shift rest down
+    public void add(int val,int index){
+
+    }
+
+    //retrieves value from specific index
+    public void get(int index){
+    }
+
+    //Set value at an index to specificed value
+    public void set(int val, int index){
+    }
+
+    //returns length of list. How many elements in list
+    public void size(){
+    }
+
+    //Remove value from index and shift values over
+    public void remove(int index){
+    }
+}
