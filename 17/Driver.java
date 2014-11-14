b50ba015d0a7bf9b9364e81d1e396b79713a32be
Henry Filosa <henryfilosa@henrys-imac.home>
2014-11-13 19:44:37 -0500

    It appears taht the algorith we implemented for the arrays is more effective than the methods of creating a new array with random sampling. The second algorith seems to scale with almost no lag. More research is required

--- 17/Driver.java
+++ 17/Driver.java
@@ -2,28 +2,39 @@ import java.util.*;
 
 public class Driver{
 
-    public String print(){
+    //A function to print the arrays
+    public String print(int[] a){
 	String ans = "";
+	for (int i=0;i<a.length;i++){
+	    ans+=a[i]+",";
+	}
 	return ans;
     }
 
     public static void main(String args[]){
 
+	//Creates an arrayList of values 0-99999
 	ArrayList<Integer> al = new ArrayList<Integer>();
 	Random r = new Random();
 	for (int i=0;i<100000;i++){
 	    al.add(i);
 	}
 
+	//	System.out.println("ArrayList Before Randomization: "+al);
+
+	//new arraylist to randomly place values into 
 	ArrayList<Integer> newAl = new ArrayList<Integer>();
 
-	for(int i=0;i<al.size();i++){
-	    newAl.add(al.remove(r.nextInt(al.size())));
+	//Randomly selects from original list and add to new list
+	for(int i=0;i<al.size();i=i){
+	    int val = al.remove(r.nextInt(al.size()));
+	    newAl.add(val);
 	}
+	al=newAl;
 
+	//	System.out.println("ArrayList After Ramdomization: "+al);	
 
-	/*
-	int[] a = new int[20];
+	int[] a = new int[100000];
 	for(int i=0;i<a.length;i++){
 	    a[i]=i;
 	}
@@ -32,17 +43,14 @@ public class Driver{
 	int secondindex = 0;
 	int firstval = 0;
 
-	//		System.out.println("Array Before Randomization: "+a);
 	for (int i=0;i<a.length*10;i++){
-	    firstindex=r.nextInt(20);
-	    secondindex=r.nextInt(20);
+	    firstindex=r.nextInt(100000);
+	    secondindex=r.nextInt(100000);
 	    firstval=a[firstindex];
 	    a[firstindex]=a[secondindex];
 	    a[secondindex]=firstval;
 	}
-
-	//	System.out.println("Array After Ramdomization: "+a);
-	*/
+	
 
     }
 }
